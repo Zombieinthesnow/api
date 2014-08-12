46ee54d0e31dadc831b63f1e808e5788357977c1
Nicholas Costa <travelsbybike@gmail.com>
2014-08-12 16:51:44 -0400

    update method complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,86 +2,132 @@ require 'nokogiri'
 require 'open-uri'
 
 class HomeController < ApplicationController
+
+
+
+
 	def index
+
 		@stations = BikeStation.all
 
+		@doc = Nokogiri::HTML(open("http://www.thehubway.com/data/stations/bikeStations.xml")) 
+
+
+
 		if @stations.count == 0
-
-			doc = Nokogiri::HTML(open("http://www.thehubway.com/data/stations/bikeStations.xml")) 
-
-			puts 'STARTS HERE'
-
-			doc.xpath("//station").each do |node|
-				id = node.xpath("id").text
-				name = node.xpath("name").text
-				terminalName = node.xpath("terminalname").text
-				lastCommWithServer = node.xpath("lastcommwithserver").text
-				lat = node.xpath("lat").text
-				long = node.xpath("long").text
-				installed = node.xpath("installed").text
-				locked = node.xpath("locked").text
-				installDate = node.xpath("installdate").text
-				removalDate = node.xpath("removaldate").text
-				temporary = node.xpath("temporary").text
-				publicStation = node.xpath("public").text
-				numOfBikes = node.xpath("nbbikes").text
-				numOfEmptyDocks = node.xpath("nbemptydocks").text
-				latestUpdateTime = node.xpath("latestupdatetime").text
+                        #initial count in the database 
 
 
-				
-				puts id
-				puts name
-				puts terminalName
-				puts lastCommWithServer
-				puts lat
-				puts long
-				puts installed
-				puts locked
-				puts installDate
-				puts removalDate      #does not display anything 
-				puts temporary 
-				puts publicStation
-				puts numOfBikes
-				puts numOfEmptyDocks
-				puts latestUpdateTime
-				puts ""
+                        puts 'STARTS HERE'
 
-				#BikeStation.create(attr:value, )
+                        parse_xml(@doc)
 
-				BikeStation.create( bike_station_id:id, name:name, terminalName:terminalName, lastCommWithServer: lastCommWithServer, lat:lat, long:long, installed:installed, locked:locked, installDate:installDate, removalDate:removalDate, temporary:temporary, publicStation:publicStation, nbBikes:numOfBikes, nbEmptyDocks:numOfEmptyDocks, latestUpdateTime:latestUpdateTime)
 
-				#another active record query 
 
-				#BikeStation.where name equals ( )  #within 5 miles 
+                end
+        else
 
-							
+            puts "Station count is: #{@stations.count}"
 
-		end
-			else
-				puts @stations.count
+            update_stations(@doc,@stations) #method 
+    end
 
-			end
-	
 
-		#root = doc.xpath("/name").text
+end
 
-		
-		#puts root
 
+
+def parse_xml(document)
+        document.xpath("//station").each do |node|
+                bikeStationid = node.xpath("id").text
+                name = node.xpath("name").text
+                terminalName = node.xpath("terminalname").text
+                lastCommWithServer = node.xpath("lastcommwithserver").text
+                lat = node.xpath("lat").text
+                long = node.xpath("long").text
+                installed = node.xpath("installed").text
+                locked = node.xpath("locked").text
+                installDate = node.xpath("installdate").text
+                removalDate = node.xpath("removaldate").text
+                temporary = node.xpath("temporary").text
+                publicStation = node.xpath("public").text
+                numOfBikes = node.xpath("nbbikes").text
+                numOfEmptyDocks = node.xpath("nbemptydocks").text
+                latestUpdateTime = node.xpath("latestupdatetime").text
+
+                BikeStation.create( bike_station_id:bikeStationid, name:name, terminalName:terminalName, lastCommWithServer:lastCommWithServer, lat:lat, long:long, installed:installed, locked:locked, installDate:installDate, removalDate:removalDate, temporary:temporary, publicStation:publicStation, nbBikes:numOfBikes, nbEmptyDocks:numOfEmptyDocks, latestUpdateTime:latestUpdateTime)
+        end
+
+end
+
+
+
+def update_stations(document,bikestations)
+
+                #this is used to update the most frequently changing station info  
+
+# update attributes  - numOfBikes, numOfEmptyDocks, latestUpdateTime, locked, lastCommWithServer,   check what is - pubicStaion, temporary
+
+
+document.xpath("//station").each do |node|
+    bsid = node.xpath("id").text  # get the id of the stations first 
+
+    last_Comm_With_Server = node.xpath("lastcommwithserver").text
+    locked = node.xpath("locked").text
+    num_Bikes = node.xpath("nbbikes").text
+    num_Empty_Docks = node.xpath("nbemptydocks").text
+    latest_Update_Time = node.xpath("latestupdatetime").text
+
+puts ""
+    puts "bike station id #{bsid}"
+
+#NOTE: make sure the order is correct 
+    # 1.find the station by id 
+    station = BikeStation.find_by(bike_station_id: bsid) 
+    #2. update all attributes of that particular station
+    station.update_attributes(lastCommWithServer:last_Comm_With_Server, locked:locked, nbBikes:num_Bikes, nbEmptyDocks:num_Empty_Docks, latestUpdateTime:latest_Update_Time ) 
+
+
+
+        
+        # I need to get the bikeStation id 
 =begin
-		def num_bikes
+        @user = User.find_by(id: 1)
+ now if you want to update the user's name and password, you can do
+either
 
-		  @num_bikes = Station.where(statio_name: params[:somevaluefromtheform])
-      	  return "hello world"
-		end
+ @user.update(name: "ABC", pass: "12345678")
+or
 
-
-	end
+ @user.update_attributes(name: "ABC", pass: "12345678")
 
 =end
 
 
-	end
+        #BikeStation.update_attributes( :nbBikes => @numOfBikes)
 
-end
+
+
+=begin 
+    bikestations.each do |bks|
+     .update_attributes( :nbBikes => @numOfBikes )
+
+     
+=end
+puts "end of update "
+puts ""
+           end #end of document loop
+                                  
+
+
+
+
+
+
+                
+
+
+                       end #end of method
+
+
+
